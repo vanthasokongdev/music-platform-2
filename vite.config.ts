b6d8bd244d5413738d7eeae2ad5290320a5fd3cc
sokong.vantha <sokong.vantha@spacianet.co.jp>
2025-10-27 11:07:40 +0700

    add gh root

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/music-platform-2/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
